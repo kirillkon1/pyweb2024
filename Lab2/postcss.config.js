a2806909cde8df0e961fa2700aa6b7d1239c2890
kirillkon1 <kirill.kondrashow@mail.ru>
2024-12-18 21:26:15 +0300

    Lab2 - Test

--- Lab2/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('cssnano')({
-      preset: 'default',
-    }),
-    // Добавьте другие плагины PostCSS при необходимости
-  ],
-};
